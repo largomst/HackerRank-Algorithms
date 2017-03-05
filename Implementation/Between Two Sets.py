8d44678cc7fb2959a9b5cbd3e1cc0195555e5a93
largomst <dingrui.zhen@gmail.com>
2017-03-05 10:20:36 +0800

    fixed factor

--- Implementation/Between Two Sets.py
+++ Implementation/Between Two Sets.py
@@ -5,20 +5,26 @@ b = [int(arr_temp) for arr_temp in input().strip().split(' ')]
 
 
 def factor(n):
-    factor_list = list()
+    factor_list = set()
     for i in range(1, n + 1):
         if n % i == 0:
-            factor_list.append(i)
+            factor_list.add(i)
     return factor_list
 
 
-factor_of_b = set()
-for elem in b:
-    for i in factor(elem):
-        factor_of_b.add(i)
+# **x** is a factor of all elements in **B**.
+
+a_factor_of_B = factor(b[0])
+
+for i in b:
+    a_factor_of_B = a_factor_of_B.intersection(factor(i))
+
+result = set()
 
 a_factor = set(a)
+for i in a_factor_of_B:
+    if a_factor.intersection(factor(
+            i)) == a_factor:  # All elements in **A** are factors of **x**.
+        result.add(i)
 
-union = factor_of_b.intersection(a_factor)
-
-print(len(union))
+print(len(result))
