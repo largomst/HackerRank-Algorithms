cfc6cc7045d4ed0fc5fd14271d5c9f0c6e919f24
largomst <dingrui.zhen@gmail.com>
2017-03-06 20:55:40 +0800

    fixed result = 0

--- Implementation/Save the Prisoner.py
+++ Implementation/Save the Prisoner.py
@@ -2,7 +2,20 @@ t = int(input().strip())
 for t0 in range(t):
     n, m, s = input().strip().split(' ')
     n, m, s = int(n), int(m), int(s)
-    s = s - 1
-    for i in range(m):
-        s = (s + 1) % n
-    print(s)
+    result = ((m + s) - 1) % n
+    if result == 0:
+        result = n
+    print(result)
+
+# error
+# 499999999 999999997 2
+# 0
+# 499999999 999999998 2
+# 1
+# 999999999 999999999 1
+# 0
+# right
+# 499999999
+# 1
+# 999999999
+# 当恰好分到最后一人时，会出现 result = 0
