42e54e7a176eb474a503cc4189af960258e21bf3
largmost <dingrui.zhen@gmail.com>
2017-03-07 12:09:42 +0800

    fixed error

--- Implementation/Jumping on the Clouds Revisited.py
+++ Implementation/Jumping on the Clouds Revisited.py
@@ -6,14 +6,10 @@ n, k = input().strip().split(' ')
 n, k = [int(n), int(k)]
 c = [int(c_temp) for c_temp in input().strip().split(' ')]
 i = 0
-start = True
 E = 100
+start = True
 while i != 0 or start:
-    if start:
-        i = (k + i) % n
-        start = False
-        continue
-    E -= (c[i] + k) % n
-    i = (k + i) % n
-
+    i = (i + k) % n
+    E -= c[i] * 2 + 1
+    start = False
 print(E)
