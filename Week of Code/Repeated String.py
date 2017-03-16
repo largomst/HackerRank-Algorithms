b3d008fad51382793c090eafb9d433482567750c
largmost <dingrui.zhen@gmail.com>
2017-03-16 16:41:39 +0800

    more elegance code

--- Week of Code/Repeated String.py
+++ Week of Code/Repeated String.py
@@ -4,23 +4,6 @@ import sys
 
 s = input().strip()
 n = int(input().strip())
-s = list(s)
-
-a_time = 0
-for i in s:
-    if i == 'a':
-        a_time += 1
-
-s_length = len(s)
-repeated_time = n // s_length
-rare_s_index = n % s_length
-rare_s = s[:rare_s_index]
-
-a_rare_time = 0
-for i in rare_s:
-    if i == 'a':
-        a_rare_time += 1
-
-a_time = a_time * repeated_time + a_rare_time
-
-print(a_time)
+time = sum([1 for i in s if i == 'a']) * (n // len(s))
+time += sum([1 for i in range(n % len(s)) if s[i] == 'a'])
+print(time)
