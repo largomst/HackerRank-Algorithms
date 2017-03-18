8da7b6788d110d7e9c2263a534b2e4d752c9ed6c
largmost <dingrui.zhen@gmail.com>
2017-03-18 18:55:51 +0800

    improved solution

--- Implementation/Equalize the Array.py
+++ Implementation/Equalize the Array.py
@@ -1,18 +1,4 @@
 n = int(input())
 arr = list(map(lambda x: int(x), input().strip().split(' ')))
 
-arr_count = dict()
-for i in arr:
-    arr_count[i] = 0
-
-for i in arr:
-    arr_count[i] += 1
-
-aim = max(arr_count.items(), key=lambda x: x[1])[0]
-
-count = 0
-for i in arr:
-    if i != aim:
-        count += 1
-
-print(count)
+print(n - max([arr.count(t) for t in set(arr)]))
