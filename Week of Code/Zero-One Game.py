2e60ec54f4729b1f873f0cdc477c3854ce29c374
largmost <dingrui.zhen@gmail.com>
2017-04-12 15:26:36 +0800

    add Zero-One Game

---
+++ Week of Code/Zero-One Game.py
@@ -0,0 +1,12 @@
+# TODO you know
+
+
+def solution(n, sequence):
+    pass
+
+
+g = int(input().strip())
+for a0 in range(g):
+    n = int(input().strip())
+    sequence = list(map(int, input().strip().split(' ')))
+    # If Alice wins, print 'Alice' on a new line; otherwise, print 'Bob'
