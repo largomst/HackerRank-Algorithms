a4359581dd161c2d6d9f8776249148872812eb0e
largmost <dingrui.zhen@gmail.com>
2017-04-30 08:21:31 +0800

    improve the solution

--- HourRank/Recover the Arrays.py
+++ HourRank/Recover the Arrays.py
@@ -5,10 +5,8 @@ file = [int(a) for a in input().strip().split(' ')]
 i = 0
 linCount = 0
 while i < n:
-    e = file[i]
-    for j in range(e):
-        i += 1
-    i += 1
+    # Skip 'i' past current array to the beginning of the next array
+    i += file[i] + 1
     linCount += 1
 
 print(linCount)
