b52cac277c4b0a92f79a82c6b580e24bed20bad0
largmost <dingrui.zhen@gmail.com>
2017-05-01 09:11:07 +0800

    add tree preorder

---
+++ Trees Challenges/Tree Preorder Traversal.py
@@ -0,0 +1,16 @@
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+
+
+def preOrder(root):
+    if root is None:
+        return
+    print(root.data, end=' ')
+    if root.left is not None:
+        preOrder(root.left)
+    if root.right is not None:
+        preOrder(root.right)
